84aa314f4a60e471f240b8d67a73652c691bec60
SUSHAN BANIYA <sushanbaniya@gmail.com>
2023-12-20 14:37:48 +0545

    added few things on tax calculation screen

--- lib/screens/tax_screen.dart
+++ lib/screens/tax_screen.dart
@@ -5,12 +5,12 @@ import '../widgets/my_drawer.dart';
 class TaxScreen extends StatefulWidget {
   static const routeName = '/tax-screen';
 
-  @override 
+  @override
   State<TaxScreen> createState() => TaxScreenState();
 }
 
 class TaxScreenState extends State<TaxScreen> {
-  @override 
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       drawer: MyDrawer(),
@@ -19,7 +19,44 @@ class TaxScreenState extends State<TaxScreen> {
         title: Text('TAX CALCULATOR'),
         toolbarHeight: 120,
       ),
-      body: Center(child: Text('screen for taxes'))
+      body: Column(
+        children: [
+          SizedBox(height: 100),
+          TextField(
+            
+            textAlign: TextAlign.center,
+            decoration: InputDecoration(
+                hintText: 'ENTER YEARLY INCOME',
+              
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.pink,
+                    width: 5,
+                  ),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(50),
+                  ),
+                ),
+                hintStyle: TextStyle(letterSpacing: 9)),
+            keyboardType: TextInputType.number,
+          ),
+          SizedBox(height: 50),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              fixedSize: Size.fromWidth(170),
+              
+              backgroundColor: Theme.of(context).primaryColorDark,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(20),
+                ),
+              ),
+            ),
+            child: Text('CALCULATE TAX !'),
+            onPressed: () {},
+          ),
+        ],
+      ),
     );
   }
 }
