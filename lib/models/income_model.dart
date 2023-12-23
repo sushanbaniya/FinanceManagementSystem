de8c9771f1e982d1ab505d8ca04e0eb51a6c50c2
SUSHAN BANIYA <sushanbaniya@gmail.com>
2023-12-23 16:20:04 +0545

    displaying the entered input of the body of scaffold

---
+++ lib/models/income_model.dart
@@ -0,0 +1,11 @@
+class IncomeModel {
+  var id;
+  var incomeSource;
+  var incomeAmount;
+
+  IncomeModel({var rId, var rIncomeSource, var rIncomeAmount}) {
+    id = rId;
+    incomeSource = rIncomeSource;
+    incomeAmount = rIncomeAmount;
+  }
+}
--- lib/screens/income_screen.dart
+++ lib/screens/income_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import '../widgets/my_drawer.dart';
+import '../models/income_model.dart';
 
 class IncomeScreen extends StatefulWidget {
   static const routeName = '/income-screen';
@@ -14,118 +15,142 @@ class IncomeScreenState extends State<IncomeScreen> {
   var incomeForm = GlobalKey<FormState>();
   var incomeSource;
   var incomeAmount;
+  var incomeList = [];
+
+  void addIncome(var incomeSource, var incomeAmount) {
+    setState(() {
+      incomeList.add(
+        IncomeModel(
+          rId: DateTime.now().toString(),
+          rIncomeSource: incomeSource,
+          rIncomeAmount: incomeAmount,
+        ),
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        drawer: MyDrawer(),
-        appBar: AppBar(
-            title: Text('INCOMES'),
-            toolbarHeight: 120,
-            backgroundColor: Theme.of(context).primaryColorLight,
-            actions: [
-              IconButton(
-                icon: Icon(FontAwesomeIcons.refresh),
-                onPressed: () {},
-              )
-            ]),
-        floatingActionButton: FloatingActionButton(
-          backgroundColor: Theme.of(context).primaryColorDark,
-          child: Icon(FontAwesomeIcons.add),
-          onPressed: () {
-            showModalBottomSheet(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(50),
+      drawer: MyDrawer(),
+      appBar: AppBar(
+        title: Text('INCOMES'),
+        toolbarHeight: 120,
+        backgroundColor: Theme.of(context).primaryColorLight,
+        actions: [
+          IconButton(
+            icon: Icon(FontAwesomeIcons.refresh),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Column(
+        children: incomeList.map((income) {
+          return Container(
+            color: Colors.black,
+            child: Column(
+              children: [
+                Text('${income.incomeSource}'),
+                Text('${income.incomeAmount}'),
+              ],
+            ),
+          );
+        }).toList(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Theme.of(context).primaryColorDark,
+        child: Icon(FontAwesomeIcons.add),
+        onPressed: () {
+          showModalBottomSheet(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(
+                Radius.circular(50),
+              ),
+            ),
+            context: context,
+            builder: (context) {
+              return Container(
+                margin: EdgeInsets.all(18),
+                child: Form(
+                  key: incomeForm,
+                  child: Column(
+                    children: [
+                      TextFormField(
+                          textAlign: TextAlign.center,
+                          cursorColor: Theme.of(context).primaryColorLight,
+                          decoration: InputDecoration(
+                            hintText: 'ENTER SOURCE OF INCOME',
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(30),
+                              ),
+                              borderSide: BorderSide(
+                                  color: Theme.of(context).primaryColorDark,
+                                  width: 2),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(30),
+                              ),
+                              borderSide: BorderSide(
+                                  color: Theme.of(context).primaryColorLight,
+                                  width: 5),
+                            ),
+                          ),
+                          textInputAction: TextInputAction.next,
+                          onSaved: (newValue) {
+                            incomeSource = newValue;
+                          }),
+                      SizedBox(height: 18),
+                      TextFormField(
+                          textAlign: TextAlign.center,
+                          cursorColor: Theme.of(context).primaryColorLight,
+                          decoration: InputDecoration(
+                            hintText: 'ENTER AMOUNT OF INCOME',
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(30),
+                              ),
+                              borderSide: BorderSide(
+                                  color: Theme.of(context).primaryColorDark,
+                                  width: 2),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(30),
+                              ),
+                              borderSide: BorderSide(
+                                  color: Theme.of(context).primaryColorLight,
+                                  width: 5),
+                            ),
+                          ),
+                          textInputAction: TextInputAction.done,
+                          keyboardType: TextInputType.number,
+                          onSaved: (newValue) {
+                            incomeAmount = newValue;
+                          }),
+                      SizedBox(height: 30),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Theme.of(context).primaryColorDark,
+                        ),
+                        child: Text('Track this Income !'),
+                        onPressed: () {
+                          incomeForm.currentState!.save();
+                          addIncome(incomeSource, incomeAmount);
+                          Navigator.of(context).pop();
+                          // print(incomeSource);
+                          // print(incomeAmount);
+                        },
+                      ),
+                    ],
                   ),
                 ),
-                context: context,
-                builder: (context) {
-                  return Container(
-                    margin: EdgeInsets.all(18),
-                    child: Form(
-                      key: incomeForm,
-                      child: Column(
-                        children: [
-                          TextFormField(
-                            textAlign: TextAlign.center,
-                            cursorColor: Theme.of(context).primaryColorLight,
-                            decoration: InputDecoration(
-                              hintText: 'ENTER SOURCE OF INCOME',
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.all(
-                                  Radius.circular(30),
-                                ),
-                                borderSide: BorderSide(
-                                    color: Theme.of(context).primaryColorDark,
-                                    width: 2),
-                              ),
-                              focusedBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.all(
-                                  Radius.circular(30),
-                                ),
-                                borderSide: BorderSide(
-                                    color: Theme.of(context).primaryColorLight,
-                                    width: 5),
-                              ),
-                            ),
-                            textInputAction: TextInputAction.next,
-
-                            onSaved: (newValue) {
-                              incomeSource = newValue;
-                            }
-                          ),
-                          SizedBox(height: 18),
-
-                          TextFormField(
-                            textAlign: TextAlign.center,
-                            cursorColor: Theme.of(context).primaryColorLight,
-                            decoration: InputDecoration(
-                              hintText: 'ENTER AMOUNT OF INCOME',
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.all(
-                                  Radius.circular(30),
-                                ),
-                                borderSide: BorderSide(
-                                    color: Theme.of(context).primaryColorDark,
-                                    width: 2),
-                              ),
-                              focusedBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.all(
-                                  Radius.circular(30),
-                                ),
-                                borderSide: BorderSide(
-                                    color: Theme.of(context).primaryColorLight,
-                                    width: 5),
-                              ),
-                            ),
-                            textInputAction: TextInputAction.done,
-                            keyboardType: TextInputType.number,
-                            onSaved: (newValue) {
-                              incomeAmount = newValue;
-                            }
-                          ),
-                          
-                          SizedBox(height: 30),
-                          ElevatedButton(  
-                            style: ElevatedButton.styleFrom(  
-                              backgroundColor: Theme.of(context).primaryColorDark,
-                            ),
-                            child: Text('Track this Income !'),
-                            onPressed: (){
-                              incomeForm.currentState!.save();
-                              Navigator.of(context).pop();
-                              print(incomeSource);
-                              print(incomeAmount);
-                            }
-                          )
-
-                        ],
-                      ),
-                    ),
-                  );
-                });
-          },
-        ));
+              );
+            },
+          );
+        },
+      ),
+    );
   }
 }
